47c8ff924d719abb179fac14e4b3e2e9d5169972
Myroslava Bokotey <mirosyabokotey@gmail.com>
2025-11-08 22:56:28 +0000

    navbar start

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import "../CSS/navbar.css";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav>
-      <h1>My Application</h1>
+    <nav className="navbar">
+      <div className="nav-brand">
+        <Link to="/home">FileLabs</Link>
+      </div>
+      
+      <div className="nav-center">
+        <button className="nav-button">Button 1</button>
+        <button className="nav-button">Button 2</button>
+        <button className="nav-button">Button 3</button>
+      </div>
+      
+      <div className="nav-right">
+        <Link to="/profile" className="profile-link">Profile</Link>
+      </div>
     </nav>
   );
 }
