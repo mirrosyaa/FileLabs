c215b29d2bc991384f93adf0287bd8a2cc75ba50
Max Ward <maxward3@outlook.com>
2025-11-08 20:26:37 +0000

    added navbar structure

---
+++ client/src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../CSS/navbar.css";
+
+function Navbar() {
+  return (
+    <nav>
+      <h1>My Application</h1>
+    </nav>
+  );
+}
+export default Navbar;
--- client/src/pages/homePage.jsx
+++ client/src/pages/homePage.jsx
@@ -1,6 +1,7 @@
 import { React } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../Authentication/authProvider";
+import Navbar from "../components/navbar";
 
 function HomePage() {
   const { setToken } = useAuth();
@@ -12,12 +13,15 @@ function HomePage() {
   };
 
   return (
-    <div className="container mt-5">
-      <h1>Welcome to the Home Page</h1>
-      <p>You are successfully logged in!</p>
-      <button className="btn btn-danger" onClick={handleLogout}>
-        Logout
-      </button>
+    <div>
+      <Navbar />
+      <div className="container mt-5">
+        <h1>Welcome to the Home Page</h1>
+        <p>You are successfully logged in!</p>
+        <button className="btn btn-danger" onClick={handleLogout}>
+          Logout
+        </button>
+      </div>
     </div>
   );
 }
