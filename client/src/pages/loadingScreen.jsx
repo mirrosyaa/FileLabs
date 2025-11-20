b3ca0ed60a47219dc23c069c5466d114e484f8d5
Max Ward <maxward3@outlook.com>
2025-11-20 14:59:04 +0000

    loggin in load

--- client/src/pages/loadingScreen.jsx
+++ client/src/pages/loadingScreen.jsx
@@ -13,7 +13,7 @@ function LoadingScreen() {
         >
           <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zM13 3.5L18.5 9H13V3.5z" />
         </svg>
-        <p className={styles["loading-text"]}>Loading...</p>
+        <p className={styles["loading-text"]}>Logging in...</p>
       </div>
     </div>
   );
